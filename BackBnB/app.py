4332387f4056b0ac597b8eb4745fc05a3a0bdded
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-10-13 14:45:45 -0400

    get crime index

--- BackBnB/app.py
+++ BackBnB/app.py
@@ -2,15 +2,22 @@
 #examples
 
 from flask import Flask, jsonify, abort, make_response, request, url_for
-
+import requests
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def home():
-    return "Welcome"
+	return "Welcome"
 
+
+def getCrimeIndex(lat,lon):
+	url = "https://crimescore.p.mashape.com/crimescore?f=json&id=174&lat=" + lat + "&lon=" + lon
+	headers = {"X-Mashape-Key": "qWMc2K59dgmshJH33sKjN5KILREOp1QQXj2jsniuCdIgcwNvTi", "Accept": "application/json"}
+	response = requests.get(url, headers = headers)
+	crime_dict = json.loads(response.text)
+	return crime_dict["score"]
 
 if __name__ == '__main__':
     app.run(debug=True)
