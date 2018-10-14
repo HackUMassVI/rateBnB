09cefc281399eb9c040271a6c15dcc67b6c53e09
ishan305 <32609035+ishan305@users.noreply.github.com>
2018-10-14 02:41:36 -0400

    Add files via upload

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,10 +1,8 @@
-//content.js
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     	var tab = tabs[0];
 	
 	var bool= tab.url.includes("airbnb.com/rooms");
 	document.querySelector(".frame1").style.display="none";
-
 
 	if(bool)
 	{
@@ -14,6 +12,7 @@
 
 	const xhttp = new XMLHttpRequest();
 	var modURL= tab.url.substr(8);
+	
 	var temp = "";
 	for (var i = 0; i < modURL.length ; i++) {
 		if(modURL.charAt(i)=='?'){
@@ -21,6 +20,11 @@
 		}
 		temp += modURL.charAt(i);
 	}
+	var modURL1="https://"+temp;
+	var finalURL= "http://67.205.146.104:5000/scoreListChrome?url="+modURL1;
+	var link = document.getElementById("redir");
+    link.setAttribute('href', finalURL);
+	
 	console.log(temp);
 	xhttp.open("GET", "http://67.205.146.104/chrome_get?url="+temp, true);
 	xhttp.onreadystatechange = function()
