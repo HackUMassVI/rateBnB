802831c672d8936da65b4fcd8cff87e2786e5dd6
mhendrick98 <mhendric@bu.edu>
2018-10-13 23:46:28 -0400

    added new route for chrome

--- website/main.py
+++ website/main.py
@@ -29,6 +29,20 @@ def home():
 def scoreList(origUrl,amenities,image,listing_name,rating,review_count,safety,score):
         return render_template('scoredListing.mako', url=origUrl, amenities=amenities,image=image,listing_name=listing_name,rating=rating,review_count=review_count,safety=safety,score=score)
 
+@app.route('/scoreListChrome')
+def scoreListChrome():
+    url = request.args.get('url')
+    origUrl = url
+    temp = len(url)
+    if "?" in url:
+        temp = url.index("?")
+    url = url[8:temp]
+    returnedVal = "http://67.205.146.104:80/get_all_info?url=" + url
+    r = requests.get(returnedVal)
+    d = r.json()
+    data = d[0]
+    return scoreList(origUrl, data['amenities'], data['image'],data['listing_name'], data['rating'], data['review_count'], data['safety'], data['score'])
+
 @app.route('/about')
 def about():
     return render_template('about.mako')
